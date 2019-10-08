af7fe0a324b585dc3a820329cdf0394e2208d759
tusharahuja14 <42529534+tusharahuja14@users.noreply.github.com>
2019-10-08 21:03:02 +0530

    Java Code for printing hello world

---
+++ hello_world.java
@@ -0,0 +1,14 @@
+package Self;
+
+import java.util.Scanner;
+
+public class misc {
+
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String args[]) {
+
+System.out.println("Hello World");
+
+	}
+}
